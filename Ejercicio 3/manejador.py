63625b10201bfe11e3b686517b744edd417abdab
Alex-FOB <103607209+Alex-FOB@users.noreply.github.com>
2022-06-24 17:20:44 -0300

    Add files via upload

---
+++ Ejercicio 3/manejador.py
@@ -0,0 +1,19 @@
+class Manejador:
+    __dolars = None
+    def __init__(self):
+        self.__dolars = []
+    def addDolar(self, dolar):
+        self.__dolars.append(dolar)
+    def calcular(self, nombre, modo, dolar):
+        pesos = 0.0
+        i = 0
+        band = False
+        while not band and i < len(self.__dolars):
+            dato = self.__dolars[i]
+            if(dato.getNom() == nombre):
+                if(modo == 0):
+                    pesos = dolar * dato.getCompra()
+                elif(modo == 1):
+                    pesos = dolar * dato.getVenta()
+            i += 1
+        return pesos
